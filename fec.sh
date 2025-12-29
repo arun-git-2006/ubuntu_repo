1e955a203c578b90cdedf15aa28e4e5552bca7aa
Arun <ak7505566405@gmail.com>
2025-12-29 05:08:58 -0500

    Update fec.sh

--- fec.sh
+++ fec.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "--------Script Name:- Factorial Script--------"
+echo "---------Author Name:-Arun ------------------"
 read -p "enter a number for fec: " num
 eval echo {1..$num}|sed 's/ /*/g'|bc
 
